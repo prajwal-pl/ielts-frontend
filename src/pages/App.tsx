dacdd86425d54bddbc5759e65d50c3518d338451
Prajwal-PL <prajwalpl096@gmail.com>
2025-01-23 22:24:15 +0530

    Adding authentication checks

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,13 @@ import { useUser } from "@clerk/clerk-react";
 import TestInterface from "../components/global/test-interface";
 
 export default function Home() {
-  const { user } = useUser();
+  const { user, isSignedIn, isLoaded } = useUser();
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  if (!isSignedIn) {
+    window.location.href = "/sign-in";
+  }
   const userId = user?.id;
   console.log(userId);
   return (
